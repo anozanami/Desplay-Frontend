a791c98aa7b144cfed176614d70d69a06249b6cf
yeochan Yoon <maguri02@naver.com>
2024-11-01 16:38:21 +0900

    test commit

--- src/recent-image.js
+++ src/recent-image.js
@@ -9,20 +9,13 @@ class App extends Component {
           <input type="text" className="search-bar" placeholder="검색어 입력" />
         </header>
         <main className="App-main">
-          {/* <div className="recent-image-container">
+          <div className="recent-image-container">
             <div className="recent-image-slot">이미지 1</div>
             <div className="recent-image-slot">이미지 2</div>
             <div className="recent-image-slot">이미지 3</div>
             <div className="recent-image-slot">이미지 4</div>
             <div className="recent-image-slot">이미지 5</div>
             <div className="recent-image-slot">이미지 6</div>
-          </div> */}
-
-          <div className="image-container">
-            <div className="image-slot">이미지 1</div>
-            <div className="image-slot">이미지 2</div>
-            <div className="image-slot">이미지 3</div>
-            <div className="image-slot">이미지 4</div>
           </div>
         </main>
       </div>
